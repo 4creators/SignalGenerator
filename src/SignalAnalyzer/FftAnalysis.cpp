d5e5b20831803e026a213a6a3842afb142c22276
4creators <djvunet@outlook.com>
2023-12-08 01:24:37 +0100

    Cleanup FFT and plotting code

--- src/SignalAnalyzer/FftAnalysis.cpp
+++ src/SignalAnalyzer/FftAnalysis.cpp
@@ -10,84 +10,89 @@
 #include "dependencies.h"
 #include "FftAnalysis.h"
 
-
-using namespace matplot;
-using namespace DevnetZone;
-
-fftw_complex* DevnetZone::GetDftFromReal(uint64_t size, double* buffer)
+#ifdef __cplusplus
+namespace DevnetZone
 {
-    if (buffer)
+#endif
+    using namespace matplot;
+
+    fftw_complex* GetDftFromReal(uint64_t size, double* buffer)
     {
-        double* inputBuffer = fftw_alloc_real(size);
-        fftw_complex* outputBuffer = fftw_alloc_complex(size);
-        if (outputBuffer)
-            memset(outputBuffer, 0, sizeof(fftw_complex) * size);
-        fftw_plan plan = fftw_plan_dft_r2c_1d((int)size, inputBuffer, outputBuffer, FFTW_ESTIMATE);
-
-        memcpy(inputBuffer, buffer, sizeof(double) * size);
-
-        fftw_execute(plan);
-
-        fftw_destroy_plan(plan);
-        fftw_free(inputBuffer);
-
-        return outputBuffer;
-    }
-
-    return nullptr;
-}
-
-void DevnetZone::GetDftXAxisFrequencies(DevnetZone::SignalGenerator& gen, uint64_t samplesCount, std::vector<double>& xAxisValues)
-{
-    double sampleFrequency = gen.SampleRate().convert_to<double>();
-    for (uint64_t k = 0; k < samplesCount; k++)
-        xAxisValues[k] = ((double)k) / samplesCount * sampleFrequency / 2;
-}
-
-void DevnetZone::PlotWaveformSpectrogramFftw(DevnetZone::SignalGenerator& gen, double* buffer)
-{
-    fftw_complex* fftBuffer = GetDftFromReal(gen.SamplesCount(), buffer);
-
-    PlotFftComplexOutput(gen, fftBuffer);
-
-    fftw_free(fftBuffer);
-}
-
-void DevnetZone::PlotFftComplexOutput(DevnetZone::SignalGenerator& gen, fftw_complex* fftBuffer)
-{
-    uint64_t fftSize = gen.SamplesCount() / 2 + 1;
-
-    double* plotBuffer = (double*)malloc(fftSize * sizeof(double));
-
-    if (plotBuffer)
-    {
-        for (int i = 0; i < fftSize; i++)
+        if (buffer)
         {
-            double real = fftBuffer[i][0];
-            double img = fftBuffer[i][1];
-            plotBuffer[i] = 2 * sqrt(pow(real, 2) + pow(img, 2)) / gen.SamplesCount(); // normalize + magnitude
+            double* inputBuffer = fftw_alloc_real(size);
+            fftw_complex* outputBuffer = fftw_alloc_complex(size);
+            if (outputBuffer)
+                memset(outputBuffer, 0, sizeof(fftw_complex) * size);
+            fftw_plan plan = fftw_plan_dft_r2c_1d((int)size, inputBuffer, outputBuffer, FFTW_ESTIMATE);
+
+            memcpy(inputBuffer, buffer, sizeof(double) * size);
+
+            fftw_execute(plan);
+
+            fftw_destroy_plan(plan);
+            fftw_free(inputBuffer);
+
+            return outputBuffer;
         }
 
-        double* endPlotBuff = plotBuffer + fftSize;
-
-        std::vector<double> x(fftSize);
-        std::vector<double> ticksX =
-        {
-            0, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 100000,
-            //11000, 12000, 13000, 14000, 15000, 16000, 17000, 18000, 19000, 200000,
-        };
-
-        GetDftXAxisFrequencies(gen, fftSize, x);
-
-        std::vector<double> y(plotBuffer, endPlotBuff);
-
-        plot(x, y);
-        axis(tight);
-        xticks(ticksX);
-
-        show();
-
-        free(plotBuffer);
+        return nullptr;
     }
-}
 
+    void GetDftXAxisFrequencies(DevnetZone::SignalGenerator& gen, uint64_t samplesCount, std::vector<double>& xAxisValues)
+    {
+        double sampleFrequency = gen.SampleRate().convert_to<double>();
+        for (uint64_t k = 0; k < samplesCount; k++)
+            xAxisValues[k] = ((double)k) / samplesCount * sampleFrequency / 2;
+    }
+
+    void PlotWaveformSpectrogramFftw(DevnetZone::SignalGenerator& gen, double* buffer)
+    {
+        fftw_complex* fftBuffer = GetDftFromReal(gen.SamplesCount(), buffer);
+
+        PlotFftComplexOutput(gen, fftBuffer);
+
+        fftw_free(fftBuffer);
+    }
+
+    void PlotFftComplexOutput(DevnetZone::SignalGenerator& gen, fftw_complex* fftBuffer)
+    {
+        uint64_t fftSize = gen.SamplesCount() / 2 + 1;
+
+        double* plotBuffer = (double*)malloc(fftSize * sizeof(double));
+
+        if (plotBuffer)
+        {
+            for (int i = 0; i < fftSize; i++)
+            {
+                double real = fftBuffer[i][0];
+                double img = fftBuffer[i][1];
+                plotBuffer[i] = 2 * sqrt(pow(real, 2) + pow(img, 2)) / gen.SamplesCount(); // normalize + magnitude
+            }
+
+            double* endPlotBuff = plotBuffer + fftSize;
+
+            std::vector<double> x(fftSize);
+            std::vector<double> ticksX =
+            {
+                0, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 100000,
+                //11000, 12000, 13000, 14000, 15000, 16000, 17000, 18000, 19000, 200000,
+            };
+
+            GetDftXAxisFrequencies(gen, fftSize, x);
+
+            std::vector<double> y(plotBuffer, endPlotBuff);
+
+            plot(x, y);
+            axis(tight);
+            xticks(ticksX);
+
+            show();
+
+            free(plotBuffer);
+        }
+    }
+
+#ifdef __cplusplus
+} // namespace DevnetZone
+#endif
--- src/SignalAnalyzer/FftAnalysis.h
+++ src/SignalAnalyzer/FftAnalysis.h
@@ -15,15 +15,23 @@
 #include "types.h"
 #include "SignalGenerator.h"
 
+#ifdef __cplusplus
 namespace DevnetZone
 {
-    extern "C" SIGNALANALYZER_API fftw_complex * GetDftFromReal(uint64_t size, double* buffer);
+    extern "C" {
+#   endif
 
-    extern "C" SIGNALANALYZER_API void PlotWaveformSpectrogramFftw(DevnetZone::SignalGenerator & gen, double* buffer);
+        SIGNALANALYZER_API fftw_complex * GetDftFromReal(uint64_t size, double* buffer);
 
-    extern "C" SIGNALANALYZER_API void PlotFftComplexOutput(DevnetZone::SignalGenerator & gen, fftw_complex * fftBuffer);
+        SIGNALANALYZER_API void PlotWaveformSpectrogramFftw(DevnetZone::SignalGenerator & gen, double* buffer);
 
-    extern "C" SIGNALANALYZER_API void GetDftXAxisFrequencies(DevnetZone::SignalGenerator & gen, uint64_t samplesCount, std::vector<double>&xAxisValues);
+        SIGNALANALYZER_API void PlotFftComplexOutput(DevnetZone::SignalGenerator & gen, fftw_complex * fftBuffer);
+
+        SIGNALANALYZER_API void GetDftXAxisFrequencies(DevnetZone::SignalGenerator & gen, uint64_t samplesCount, std::vector<double>&xAxisValues);
+
+#   ifdef __cplusplus
+    } // extern "C"
 }
+#endif // __cplusplus
 
 #endif // FFTANALYSIS_H
